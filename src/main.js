8fc9d99e5ea2e395b079b02c4372ad8335b1a888
zppro <zpprozppro@gmail.com>
2016-12-01 11:02:18 +0800

    add pull down refresh

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import moment from 'moment'
 import VueResource from 'vue-resource'
 import { sync } from 'vuex-router-sync'
-import { Progress, Spinner, Popup, Cell, Field, Badge, Actionsheet, DatetimePicker, Swipe, SwipeItem, InfiniteScroll, Lazyload } from 'mint-ui'
+import { Progress, Spinner, Popup, Cell, Field, Badge, Actionsheet, DatetimePicker, Swipe, SwipeItem, Loadmore, InfiniteScroll, Lazyload } from 'mint-ui'
 import store from './store'
 import router from './router'
 import * as httpOption from './config/http-option'
@@ -24,6 +24,7 @@ Vue.component(Actionsheet.name, Actionsheet)
 Vue.component(DatetimePicker.name, DatetimePicker)
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
+Vue.component(Loadmore.name, Loadmore)
 Vue.use(InfiniteScroll)
 Vue.use(Lazyload)
 // Vue.use(VeeValidate, veeValidateOption)
--- src/views/ExperienceHotList.vue
+++ src/views/ExperienceHotList.vue
@@ -5,21 +5,24 @@
       p(v-show="showExperienceFetchIndicator" class="page-refresh-loading")
         mt-spinner(type="triple-bounce" color="#ea5513")
         | {{dataRefreshText}}
-      experience-list.experience-list
-        experience-item(v-for="experience in experiencesHot", :experience="experience")
-          img(:src="experience.member_head_portrait || defaultMemberHeadPortrait" slot="member_head_portrait")
-          span(slot="member_name") {{experience.member_name}}
-          span(slot="time_description") {{experience.time_description}}
-          div(slot="content" v-html="experience.content")
-          span.text-danger(slot="details-link"  v-if="isExperienceRoute(experience)") 全文
-          .img-list(slot="imgs")
-            image-collection(:all-images="experience.imgs", v-on:select="zoomIn")
-          span(slot="location") {{experience.location}}
-          span(slot="retweets" v-if="experience.retweets > 0") {{experience.retweets}}
-          span(slot="stars" v-if="experience.stars > 0") {{experience.stars}}
-          span(slot="likes" v-if="experience.likes > 0") {{experience.likes}}
-          div(slot="retweetRoot" v-if="experience.retweet_flag" )
-            experience-item-retweet-root(:experience="experience.retweet_root", v-on:select="zoomIn")
+      mt-loadmore(:top-method="loadTop" @top-status-change="handleTopChange" ref="hotList")
+        experience-list.experience-list
+          experience-item(v-for="experience in experiencesHot", :experience="experience")
+            img(:src="experience.member_head_portrait || defaultMemberHeadPortrait" slot="member_head_portrait")
+            span(slot="member_name") {{experience.member_name}}
+            span(slot="time_description") {{experience.time_description}}
+            div(slot="content" v-html="experience.content")
+            span.text-danger(slot="details-link"  v-if="isExperienceRoute(experience)") 全文
+            .img-list(slot="imgs")
+              image-collection(:all-images="experience.imgs", v-on:select="zoomIn")
+            span(slot="location") {{experience.location}}
+            span(slot="retweets" v-if="experience.retweets > 0") {{experience.retweets}}
+            span(slot="stars" v-if="experience.stars > 0") {{experience.stars}}
+            span(slot="likes" v-if="experience.likes > 0") {{experience.likes}}
+            div(slot="retweetRoot" v-if="experience.retweet_flag" )
+              experience-item-retweet-root(:experience="experience.retweet_root", v-on:select="zoomIn")
+        .mint-loadmore-top.text-muted(slot="top")
+          span(v-show="topStatus !== 'loading'") 下拉刷新
       p(v-show="showExperienceAppendIndicator" class="page-append-loading")
         mt-spinner(type="fading-circle" color="#ea5513")
         | {{dataAppendText}}
@@ -38,6 +41,7 @@
   export default {
     data () {
       return {
+        topStatus: '',
         allImages: [],
         currentImage: ''
       }
@@ -56,6 +60,14 @@
       })
     },
     methods: {
+      handleTopChange (status) {
+        this.topStatus = status
+      },
+      loadTop (id) {
+        this.fetchHotList().then(() => {
+          this.$refs.hotList.onTopLoaded(id)
+        })
+      },
       zoomIn (allImages, currentImage) {
         this.allImages = allImages
         this.currentImage = currentImage
@@ -100,6 +112,9 @@
       height: 100%;
       background-color: #000;
       backface-visibility: hidden;
+    }
+    .mint-loadmore-top {
+      font-size: 0.8rem;
     }
   }
 </style>
