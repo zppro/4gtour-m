de60c125024877e758d792da4a93cccdcd583b85
zppro <zpprozppro@gmail.com>
2017-01-04 10:54:56 +0800

    setting

--- src/config/socket-option.js
+++ src/config/socket-option.js
@@ -1,3 +1,3 @@
 // 'http://192.168.101.7:3002/me-services' 'http://192.168.255.116:3002/me-services'
 export const memberSocketUrl = process.env.NODE_ENV === 'development' ? 'http://192.168.255.111:3002/member' : 'http://sh.okertrip.com/member'
-export const groupSocketUrl = process.env.NODE_ENV === 'development' ? 'http://192.168.255.107:3002/group' : 'http://sh.okertrip.com/group'
+export const groupSocketUrl = process.env.NODE_ENV === 'development' ? 'http://192.168.255.107:3002/group' : 'http://139.224.70.118:3002/group'
